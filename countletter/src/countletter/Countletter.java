5587603a1099a483df1e21690d2cfc6fa640653a
Sunshine <3538545066@163.com>
2019-11-27 20:30:23 +0800

    代码

---
+++ countletter/src/countletter/Countletter.java
@@ -0,0 +1,26 @@
+
+package countletter;
+import java.util.*;
+public class Countletter {
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);  
+      System.out.println("Enter a string :");
+      String s = input.nextLine();
+      int []  counts = countLetters(s.toLowerCase());
+      for(int i = 0;i<counts.length;i++)
+      {
+          if(counts[i] !=0)
+              System.out.println((char)('a' + i) + " appears "+ counts[i] + ((counts[i] == 1) ? "time" :" times");
+      }
+    }
+    public static int[] countLetters(String s){
+        int []  counts  = new int[26];
+        for(int i =0;i< s.length();i++)
+        {
+            if(Character.isLetter(s.charAt(i)))
+                counts[s.charAt(i)-'a']++;
+        }
+        return counts;
+    }
+    
+}
